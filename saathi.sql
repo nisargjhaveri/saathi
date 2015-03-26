dabb2dd8e08943b877ae86771b6ccbe3c52cf221
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2015-03-26 21:14:21 +0530

    DB structure

---
+++ saathi.sql
@@ -0,0 +1,276 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.0-dev
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 26, 2015 at 09:13 PM
+-- Server version: 5.5.41-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `saathi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assets`
+--
+
+CREATE TABLE IF NOT EXISTS `assets` (
+  `id` int(11) NOT NULL,
+  `organisation_id` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `camps`
+--
+
+CREATE TABLE IF NOT EXISTS `camps` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `organisation_id` int(11) NOT NULL,
+  `camp_head` int(11) NOT NULL,
+  `population` text NOT NULL,
+  `volunteers` text NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `contact_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_details`
+--
+
+CREATE TABLE IF NOT EXISTS `contact_details` (
+  `id` int(11) NOT NULL,
+  `phone_no` varchar(15) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mailing_list` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `missing_persons`
+--
+
+CREATE TABLE IF NOT EXISTS `missing_persons` (
+  `id` int(11) NOT NULL,
+  `person_id` int(11) NOT NULL,
+  `body_marks` text NOT NULL,
+  `height` varchar(255) NOT NULL,
+  `hair` varchar(255) NOT NULL,
+  `eye_color` varchar(255) NOT NULL,
+  `weight` varchar(255) NOT NULL,
+  `last_seen` text NOT NULL,
+  `status` text NOT NULL,
+  `contact_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organisatios`
+--
+
+CREATE TABLE IF NOT EXISTS `organisatios` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `home_country` varchar(255) NOT NULL,
+  `founded` varchar(255) NOT NULL,
+  `logo` longblob NOT NULL,
+  `description` text NOT NULL,
+  `contact_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `persons`
+--
+
+CREATE TABLE IF NOT EXISTS `persons` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` int(11) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `dob` date NOT NULL,
+  `photo` longblob NOT NULL,
+  `contact_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE IF NOT EXISTS `requests` (
+  `id` int(11) NOT NULL,
+  `organisation_id` int(11) NOT NULL,
+  `asset_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `priority` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `assets`
+--
+ALTER TABLE `assets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `organisation_id` (`organisation_id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `camps`
+--
+ALTER TABLE `camps`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `contact_id` (`contact_id`),
+  ADD KEY `camp_head` (`camp_head`),
+  ADD KEY `organisation_id` (`organisation_id`);
+
+--
+-- Indexes for table `contact_details`
+--
+ALTER TABLE `contact_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `missing_persons`
+--
+ALTER TABLE `missing_persons`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `contact_id` (`contact_id`),
+  ADD KEY `person_id` (`person_id`),
+  ADD KEY `person_id_2` (`person_id`);
+
+--
+-- Indexes for table `organisatios`
+--
+ALTER TABLE `organisatios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `contact_id` (`contact_id`);
+
+--
+-- Indexes for table `persons`
+--
+ALTER TABLE `persons`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `contact_id` (`contact_id`);
+
+--
+-- Indexes for table `requests`
+--
+ALTER TABLE `requests`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `organisation_id` (`organisation_id`),
+  ADD KEY `asset_id` (`asset_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `assets`
+--
+ALTER TABLE `assets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `camps`
+--
+ALTER TABLE `camps`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `contact_details`
+--
+ALTER TABLE `contact_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `missing_persons`
+--
+ALTER TABLE `missing_persons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `organisatios`
+--
+ALTER TABLE `organisatios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `persons`
+--
+ALTER TABLE `persons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `requests`
+--
+ALTER TABLE `requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `assets`
+--
+ALTER TABLE `assets`
+  ADD CONSTRAINT `assets_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `contact_details` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `camps`
+--
+ALTER TABLE `camps`
+  ADD CONSTRAINT `camps_ibfk_3` FOREIGN KEY (`organisation_id`) REFERENCES `organisatios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `camps_ibfk_1` FOREIGN KEY (`camp_head`) REFERENCES `persons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `camps_ibfk_2` FOREIGN KEY (`contact_id`) REFERENCES `contact_details` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `missing_persons`
+--
+ALTER TABLE `missing_persons`
+  ADD CONSTRAINT `missing_persons_ibfk_2` FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `missing_persons_ibfk_1` FOREIGN KEY (`contact_id`) REFERENCES `contact_details` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `organisatios`
+--
+ALTER TABLE `organisatios`
+  ADD CONSTRAINT `organisatios_ibfk_1` FOREIGN KEY (`contact_id`) REFERENCES `contact_details` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `persons`
+--
+ALTER TABLE `persons`
+  ADD CONSTRAINT `persons_ibfk_1` FOREIGN KEY (`contact_id`) REFERENCES `contact_details` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `requests`
+--
+ALTER TABLE `requests`
+  ADD CONSTRAINT `requests_ibfk_2` FOREIGN KEY (`organisation_id`) REFERENCES `organisatios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`asset_id`) REFERENCES `assets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
