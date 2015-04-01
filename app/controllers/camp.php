5302139c6f2e3fb0b22e76d991dfe60e342d4b99
Sourav <souravmothers153@gmail.com>
2015-04-01 17:27:38 +0530

    Camp Details can now be viewed

---
+++ app/controllers/camp.php
@@ -0,0 +1,48 @@
+<?php
+
+class camp extends Controller {
+
+    function __construct() {
+        $this->load_model('camp_model');
+    }
+
+    function index() {
+        $this->load_view('camp/index');
+    }
+
+    function add_new() {
+       $added = null;
+      
+
+        if (isset($_POST['add_new'])) {
+            $added = $this->camp_model->add_new(
+                $_POST['name'],
+                $_POST['organisation_id'],
+                $_POST['camp_head'],
+                $_POST['population'],
+                $_POST['volunteers'],
+                $_POST['status'],
+                $_POST['contact_id']
+               
+            );
+        }
+        
+        $this->load_view('camp/add_new', array(
+            'added' => $added
+            
+        ));
+        
+    }
+
+    function view() {
+        
+        $camp_list = false;
+
+        $camp_list = $this->camp_model->get_camps();
+            
+        $this->load_view('camp/view', array(
+            'camp_list' => $camp_list
+        ));
+    }
+
+}
