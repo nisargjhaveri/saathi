66e586ed41bdb6278c3b9b66efe42c0d14c455d9
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2015-04-18 00:33:26 +0530

    Redirect to error message when config is not found

---
+++ app/views/need_config.php
@@ -0,0 +1,5 @@
+<h2>Welcome to Saathi</h2>
+<h3>How to setup?</h3>
+See the documentation <a href="https://github.com/nisargjhaveri/saathi">here</a>.
+<div style="font-weight:bold">OR</div>
+Use the automatic setup script <a href="setup">here</a>.
